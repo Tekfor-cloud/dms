75e47e4a485d953ef48884983f1568efa7f1ddc3
Simon COLLIN <simon.collin@tekfor.cloud>
2023-04-11 12:21:14 +0200

    [storage_swift]

---
+++ dms_storage_swift/models/storage.py
@@ -0,0 +1,67 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError, UserError
+
+from ..utils.connection import get_swift_connection
+
+
+class Storage(models.Model):
+    _inherit = "dms.storage"
+
+    save_type = fields.Selection(selection_add=[("swift", "Swift")])
+
+    swift_recovery_directory_id = fields.Many2one(
+        "dms.directory", domain="[('storage', '=', active_id)]"
+    )
+
+    def swift_recovery(self):
+        self.ensure_one()
+
+        if not self.swift_recovery_directory_id:
+            raise UserError(
+                _("{} must be defined").format(
+                    self._fields["swift_recovery_directory_id"].string
+                )
+            )
+        elif self.swift_recovery_directory_id.storage != self:
+            raise UserError(
+                _("{} must be a folder of current storage").format(
+                    self._fields["swift_recovery_directory_id"].string
+                )
+            )
+
+        self.with_delay()._swift_recovery()
+
+    def _swift_recovery(self):
+        swift_object_list = self.storage_files.mapped("swift_object")
+        conn = get_swift_connection()
+        for swift_object in conn.get_container(self.name, full_listing=True)[1]:
+            if swift_object["name"] not in swift_object_list:
+                self.env["dms.file"].create(
+                    {
+                        "name": swift_object["name"],
+                        "directory_id": self.swift_recovery_directory_id.id,
+                        "swift_object": swift_object["name"],
+                    }
+                )
+
+    @api.model
+    def create(self, values):
+        rec = super(Storage, self).create(values)
+        if rec.save_type == "swift":
+            conn = get_swift_connection()
+            conn.put_container(values["name"])
+        return rec
+
+    @api.multi
+    def write(self, values):
+        res = super(Storage, self).write(values)
+        if values.get("save_type") == "swift":
+            conn = get_swift_connection()
+            for rec in self:
+                conn.put_container(rec.name)
+
+        if values.get("name"):
+            for rec in self.filtered(lambda r: r.save_type == "swift"):
+                raise ValidationError(_("A swift storage can't be rename"))
+
+        return res
---
+++ dms_storage_swift/utils/connection.py
@@ -0,0 +1,56 @@
+import swiftclient
+from redis import Redis
+from odoo.tools import config
+from odoo.tools.config import config
+
+
+def get_redis_connection():
+    return Redis(
+        port=config.get_misc("redis", "port", 6380),
+        host=config.get_misc("redis", "host", "localhost"),
+    )
+
+
+class SessionSaveConnection(swiftclient.client.Connection):
+    """
+    Heritage de swiftclient.client pour pemettre la sauvegarde
+    du token et de l'url de la connexion dans redis
+    """
+
+    def _retry(self, reset_func, func, *args, **kwargs):
+        res = super(SessionSaveConnection, self)._retry(
+            reset_func, func, *args, **kwargs
+        )
+        redis = get_redis_connection()
+        redis.mset(
+            {
+                "swift_token_preauthurl": self.url,
+                "swift_token_preauthtoken": self.token,
+            }
+        )
+
+        return res
+
+
+def get_swift_connection():
+    cnx_param = {
+        "authurl": config.get("swift_auth_url"),
+        "user": config.get("swift_user"),
+        "key": config.get("swift_key"),
+        "auth_version": "3",
+        "os_options": {
+            "tenant_id": config.get("swift_tenant_id"),
+        },
+    }
+    if config.get("swift_region"):
+        cnx_param["os_options"]["region_name"] = config.get("swift_region")
+
+    redis = get_redis_connection()
+    preauth = redis.mget(["swift_token_preauthurl", "swift_token_preauthtoken"])
+    if all(preauth):
+        cnx_param["preauthurl"] = preauth[0].decode()
+        cnx_param["preauthtoken"] = preauth[1].decode()
+
+    cnx = SessionSaveConnection(**cnx_param)
+
+    return cnx
