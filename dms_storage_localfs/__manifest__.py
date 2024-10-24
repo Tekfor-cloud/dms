54e2a752d72ee7f326193d98a1f7db1c859c8b4f
Simon COLLIN <simon.collin@tekfor.cloud>
2024-10-24 15:18:11 +0200

    add localfs and swift to 16.0

---
+++ dms_storage_localfs/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 TEKFor
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "DMS Storage localfs",
+    "summary": "Add localfs type of storage",
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "TEKFor SAS",
+    "website": "https://github.com/Tekfor-cloud/dms",
+    "depends": ["dms"],
+    "data": [
+        "views/storage.xml",
+    ],
+}
